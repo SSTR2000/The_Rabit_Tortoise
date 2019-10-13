fe72995e227a47e00ac89dc3bed8f4ba5b8c779b
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-10-13 13:50:27 +0530

    rabit has came

---
+++ src/main/RacingRabit.java
@@ -0,0 +1,40 @@
+package main;
+
+public class RacingRabbit
+        extends Thread {
+    public RacingRabbit(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName()
+                + " has started racing.");
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            System.out.println(getName()
+                    + " was interrupted.");
+        }
+        System.out.println(getName()
+                + " has passed the oak tree.");
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            System.out.println(getName()
+                    + " was interrupted.");
+        }
+        System.out.println(getName()
+                + " is at the top " +
+                "of the hill and " +
+                "is falling asleep.");
+        try {
+            Thread.sleep(20000L);
+        } catch (InterruptedException e) {
+            System.out.println(getName()
+                    + " was interrupted.");
+        }
+        System.out.println(getName()
+                + " has finished.");
+    }
+}
